d9b6bf6c905c36cf72cc3b9c7ab8f62df8a33147
Ben Miller <bmiller@bmiller-desktop.(none)>
2009-09-04 15:00:10 -0400

    attempting to fix repo

--- lib/generators/dcov.rb
+++ lib/generators/dcov.rb
@@ -16,7 +16,7 @@ module MetricFu
         Dir.mkdir(MetricFu::Dcov.metric_directory)
         
         #what files are we going to test
-        test_files = MetricFu.dcov[:test_files].join(' ')
+        test_files =  MetricFu.dcov[:test_files].join(' ')
         
         
         #set Dcov Options (See dcov rdocs)
